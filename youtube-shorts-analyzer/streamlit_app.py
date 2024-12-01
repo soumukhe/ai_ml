614ee2ed4cf2f535b41a16acbe5d4ea88c004f4d
soumukhe <soumukhe@gmail.com>
2024-12-01 01:51:54 -0600

    Update and rename streamline_app.py to streamlit_app.py

---
+++ youtube-shorts-analyzer/streamlit_app.py
@@ -0,0 +1,20 @@
+please modify code for streamlit_app.py to run in thread like the example below:
+
+import subprocess
+import sys
+
+# Example of detaching the process
+if len(sys.argv) == 1:
+    # Start a new instance of this script with a command line argument to prevent recursion
+    subprocess.Popen([sys.executable, __file__, 'run_in_background'])
+    sys.exit()
+else:
+    # Your existing code
+    from threading import Thread
+    import os
+
+def run_streamlit():
+  os.system('streamlit run app_adopt_barrier.py --server.port 8501')
+  
+thread = Thread(target=run_streamlit)
+thread.start()  
