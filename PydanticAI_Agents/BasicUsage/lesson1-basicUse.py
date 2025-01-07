a9a29a85c964bc1181455bc0d797c1a935a89ef0
soumukhe <soumukhe@gmail.com>
2025-01-07 10:01:28 -0600

    Update lesson1-basicUse.py

--- PydanticAI_Agents/BasicUsage/lesson1-basicUse.py
+++ PydanticAI_Agents/BasicUsage/lesson1-basicUse.py
@@ -1,6 +1,22 @@
 """
 This example is a basic example showing structure 
 
+Outputs:
+
+--------------------------------
+result: RunResult(_all_messages=[ModelRequest(parts=[SystemPromptPart(content='You are a helpful assistant', part_kind='system-prompt'), 
+UserPromptPart(content='Hello', timestamp=datetime.datetime(2025, 1, 7, 15, 59, 43, 428102, tzinfo=datetime.timezone.utc), 
+part_kind='user-prompt')], kind='request'), ModelResponse(parts=[TextPart(content='Hello! How can I assist you today?', 
+part_kind='text')], timestamp=datetime.datetime(2025, 1, 7, 15, 59, 43, tzinfo=datetime.timezone.utc),
+kind='response')], _new_message_index=0, data='Hello! How can I assist you today?',
+_usage=Usage(requests=1, request_tokens=17, response_tokens=10, total_tokens=27, 
+details={'accepted_prediction_tokens': 0, 'audio_tokens': 0, 'reasoning_tokens': 0, 'rejected_prediction_tokens': 0, 'cached_tokens': 0}))
+--------------------------------
+type: <class 'pydantic_ai.result.RunResult'>
+--------------------------------
+result.data: Hello! How can I assist you today?
+(pydantic) (base) SOUMUKHE-M-2DJG:pydantic-Vichu so
+
 """
 import os
 # Set Logfire to ignore warnings before any other imports
